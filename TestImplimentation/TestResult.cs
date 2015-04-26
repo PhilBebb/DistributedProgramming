137ad6a4c5a8b63de19ba1fb827a2101d836c830
philip <p.s.bebbington@gmail.com>
2015-04-26 14:36:58 +0100

    Added file that got left out

---
+++ TestImplimentation/TestResult.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Interfaces.Shared;
+
+namespace TestImplimentation
+{
+	public class TestResult : IResult
+	{
+		public TestResult (bool success, IDictionary<string, string> result, IJob request)
+		{
+			Success = success;
+			Result = result;
+			Request = request;
+		}
+
+		public bool Success {
+			get;
+			private set;
+		}
+
+		public IDictionary<string, string> Result {
+			get;
+			private set;
+		}
+
+		public IJob Request {
+			get;
+			private set;
+		}
+
+	}
+}
+
